f1986937449c5fe56173d8674c9fd4f0ffb597f5
Babilinx <babilinx.evx1o@simplelogin.com>
2023-06-28 11:32:02 +0200

    feat: Update post-install var description

--- src/post-install.sh
+++ src/post-install.sh
@@ -3,16 +3,17 @@
 # this script execute to apply changes to installed system from data from config-install.sh
 #
 # list variable:
-#  $ROOT       - directory where system is installed
-#  $HOSTNAME   - hostname
-#  $TIMEZONE   - timezone (xxxx/yyyy)
-#  $KEYMAP     - keymap
-#  $USERNAME   - user's login name
-#  $USER_PSWD  - user's password
-#  $ROOT_PSWD  - root's password
-#  $LOCALE     - locale
-#  $BOOTLOADER - disk to install grub (either '/dev/sdX or skip)
-#  $EFI_SYSTEM - 1 if boot in UEFI mode
+#  $ROOT         - directory where system is installed
+#  $HOSTNAME     - hostname
+#  $TIMEZONE     - timezone (xxxx/yyyy)
+#  $KEYMAP       - keymap
+#  $USERNAME     - user's login name
+#  $USER_PSWD    - user's password
+#  $ROOT_PSWD    - root's password
+#  $LOCALE       - locale
+#  $BOOTLOADER   - disk to install grub (either '/dev/sdX or skip)
+#  $BOOTLOADER_T - bootloader software
+#  $EFI_SYSTEM   - 1 if boot in UEFI mode
 #
 
 # User creation
